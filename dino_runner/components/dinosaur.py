34754775b846d6973efceb3ab802157d9899201d
Lucas Sampaio <111385611+Like-a-stone@users.noreply.github.com>
2023-03-17 17:47:58 -0300

    Add duck example
    This is a duck example for teacher.

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -15,6 +15,7 @@ class Dinosaur:
         
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.step_index = 0
         self.jump_vel = JUMP_VEL
     
@@ -40,21 +41,41 @@ class Dinosaur:
             self.dino_rect.y = Y_POS
             self.jump_vel = JUMP_VEL
     
-    def duck():
-        pass
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS + 35
+        self.step_index+=1
+        self.dino_duck = False
+
+        if self.step_index >= 10:
+            self.step_index = 0
+        
+
         
     
     def update(self, user_input):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+        
+        elif self.dino_jump:
+            self.jump()
+
+        if user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_duck = True
+            self.dino_run = False
+
+        elif not self.dino_duck and not self.dino_jump:
             self.dino_run = True
-            
+
+
         if self.dino_run:
             self.run()
-        elif self.dino_jump:
-            self.jump()                
+        elif self.dino_duck:
+            self.duck()    
+                           
             
     
     def draw(self, screen):
