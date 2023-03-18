d41eecdf820a01b01f72cd7858523a33ca923428
Lucas Sampaio <111385611+Like-a-stone@users.noreply.github.com>
2023-03-17 21:51:16 -0300

    Organizando o código
    -Step_index incluído apenas em um método.
    - Removidas linhas desnecessárias

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -12,7 +12,6 @@ class Dinosaur:
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
-        
         self.dino_run = True
         self.dino_jump = False
         self.dino_duck = False
@@ -25,13 +24,9 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index+=1        
-        
-        if self.step_index >= 10:
-            self.step_index = 0
-    
+       
     def jump(self):
         self.image = JUMPING
-        
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel*4
             self.jump_vel -=0.8
@@ -51,32 +46,27 @@ class Dinosaur:
 
         if self.step_index >= 10:
             self.step_index = 0
-        
-
-        
     
     def update(self, user_input):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        
-        elif self.dino_jump:
-            self.jump()
 
         if user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
-
         elif not self.dino_duck and not self.dino_jump:
             self.dino_run = True
-
 
         if self.dino_run:
             self.run()
         elif self.dino_duck:
-            self.duck()    
-                           
-            
+            self.duck()  
+        elif self.dino_jump:
+            self.jump()      
+
+        if self.step_index >= 10:
+            self.step_index = 0    
     
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x,self.dino_rect.y))
